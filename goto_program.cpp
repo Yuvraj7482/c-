30d56a9cda4a1dc6192a056dd11d3adc5f5cbe34
Yuvraj7482 <yuvrajsharma6793@gmail.com>
2025-05-30 21:28:30 +0530

    Add files via upload

---
+++ goto_program.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num;
+	
+	cout<<"enter any number"<<endl;
+	cin>>num;
+	
+	if(num==5)
+	{
+		goto a;
+	}
+	
+	cout<<"hello student"<<endl;
+	cout<<"good evening"<<endl;
+	a:cout<<"thank you"<<endl;
+}

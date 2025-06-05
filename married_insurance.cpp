8543af9596d5bae117155d504a0cd8e6fbf758a2
Yuvraj7482 <yuvrajsharma6793@gmail.com>
2025-06-05 23:18:09 +0530

    Add files via upload

---
+++ married_insurance.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	char y,n,choice;
+	
+	cout<<"enter married status"<<endl;
+	cin>>choice;
+	
+	if(choice=='y')
+	{
+		cout<<"insurance approved";
+	}
+	else
+	{
+		cout<<"insusrance not approved";
+	}
+}

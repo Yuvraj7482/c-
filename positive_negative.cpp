7eda7821ce85e83a4501ecc1b45962d2190c5647
Yuvraj7482 <yuvrajsharma6793@gmail.com>
2025-06-23 07:08:05 +0530

    Add files via upload

---
+++ positive_negative.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num;
+	
+	cout<<"enter number		";
+	cin>>num;
+	
+	if(num>0)
+	{
+		cout<<"num is positive";
+	}
+	else
+	{
+		cout<<"number is negatve";
+	}
+}
